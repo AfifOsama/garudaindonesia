1077e6bc37f20ace7fe7c87446221c1b0f9a2c56
afif osama <afifosama66@gmail.com>
2018-05-07 09:16:31 +0700

    second

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,18 +1,21 @@
 <?php
+
 class User extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model("User_m");
+	}
 	public function form(){
-		$this->load->view('form_user_v'); 
-  
+		$this->load->view('form_User_v'); 
   }
   public function add(){
-  	//get data
-  	echo $this->input->post("username"),"<br/>";
-  	
-  	echo $this->input->post("password"),"<br/>";
-  	
-  	echo $this->input->post("fullname"),"<br/>";
-  	
-  	echo $this->input->post("level"),"<br/>" ;
-	
+	$data = array(
+		"username" => $this->input->post("username"),
+		"password" => $this->input->post("password"),
+		"fullname" => $this->input->post("fullname"),
+		"level" => $this->input->post("level")  
+	);
+	var_dump($data);
+	$this->User_m->add($data);	
   }
 }
